4415ad1e985c8a42f9a39c2807b0ade169029779
awsmudaliar <83606881+awsmudaliar@users.noreply.github.com>
2021-06-05 08:07:14 -0600

    Adds save status of users inputs

--- speed_template/speed/game/buffer.py
+++ speed_template/speed/game/buffer.py
@@ -1,3 +1,19 @@
-from game.actor import Actor
-class Buffer(Actor):
-    pass
+import sys
+from asciimatics.event import KeyboardEvent
+
+class Buffer: 
+    
+    """ Saves Input Values """
+    def input_library(self):
+        event = self._screen.get_key()
+        input_words_list = []
+        
+        if event is None:
+            """Compare the values of the list to user inputs"""
+            final_input = self.guessed_word.join(input_words_list)
+            """ Compares saved value to word bank """
+            
+
+        elif event is not None:
+            """ Appends users input to list """
+            self.input_words_list.append(event)
